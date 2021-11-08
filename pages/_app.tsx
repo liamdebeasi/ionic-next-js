1298a2905da60d24f94bfa97b5b0e9a49928c2cc
Liam DeBeasi <liamdebeasi@icloud.com>
2021-11-08 16:57:57 -0500

    remove ionic

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,43 +1,25 @@
 import React, { useEffect } from 'react'
-import { defineCustomElements as ionDefineCustomElements } from '@ionic/core/loader'
-import { IonHeader, IonToolbar, IonTitle } from '@ionic/react';
-/* Core CSS required for Ionic components to work properly */
-import '@ionic/core/css/core.css'
 
-/* Basic CSS for apps built with Ionic */
-import '@ionic/core/css/normalize.css'
-import '@ionic/core/css/structure.css'
-import '@ionic/core/css/typography.css'
+const Accordion = class extends HTMLElement {
+  constructor() {
+    super();
+  }
+}
 
-/* Optional CSS utils that can be commented out */
-import '@ionic/core/css/padding.css'
-import '@ionic/core/css/float-elements.css'
-import '@ionic/core/css/text-alignment.css'
-import '@ionic/core/css/text-transformation.css'
-import '@ionic/core/css/flex-utils.css'
-import '@ionic/core/css/display.css'
+const defineCustomElements = (win: Window) => {
+  if (typeof (win as any) !== 'undefined') {
+    win.customElements.define('ion-accordion', Accordion);
+  }
+}
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
-    ionDefineCustomElements(window)
+    defineCustomElements(window)
   })
   return (
-    <ion-app>
-      <IonHeader translucent>
-        <IonToolbar>
-          <IonTitle>Next.js with Ionic</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-
-      <ion-content fullscreen>
-        <Component {...pageProps} />
-      </ion-content>
-      <ion-footer>
-        <ion-toolbar>
-          <ion-title>Footer</ion-title>
-        </ion-toolbar>
-      </ion-footer>
-    </ion-app>
+    <div>
+      Hello World
+    </div>
   )
 }
 
